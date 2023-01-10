ac223d686e056d4d2b3fd4ef082be47d10eca0d1
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 22:23:13 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
-import './assets/tables.scss'
-
 import Vue from 'vue'
-import VueParticles from './vue-particles'
-import App from './index'
-
-
+import VueParticles from 'vue-particles'
 Vue.use(VueParticles)
-
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#App',
-  components: { App },
-  template: '<App/>'
-  
-})
