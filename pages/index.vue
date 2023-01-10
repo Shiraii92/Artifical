d9249c80ba7775168d589caf74b771c54cd7d20d
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 20:08:19 +0000

    Update 2 files

--- pages/index.vue
+++ pages/index.vue
@@ -64,7 +64,7 @@
 
     <!-- Crypto statistic section -->
     <section
-      class="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-50 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12"
+      class=""
     >
       <div class="w-full flex flex-col lg:flex-row items-center justify-center">
         <LandingCryptoStatistic
