6b041394f7526749545c0cfe04775fece353d5b6
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 21:07:27 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue'
-
+import App from './App'
 
 import VueParticles from './vue-particles'
+Vue.use(VueParticles)
+
 Vue.config.productionTip = false
 
-import App from './App'
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
