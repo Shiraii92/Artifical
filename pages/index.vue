193a7b5525b359ef797049c2bb7b4d8a3c9ad2da
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 17:47:57 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full">
     <!-- Hero section -->
-    <section id="hero" class="md:justify-end flex flex-col justify-center pt-20 bg-gradient-to-bl from brand to-yellow-500 h-screen pb-8 lg:overflow-hidden relative">
+    <section id="hero" class="md:justify-end flex flex-col justify-center pt-0 bg-gradient-to-bl from brand to-yellow-0 h-screen pb-20 lg:overflow-hidden relative">
       <BaseSection>
         <div class="col-span-12 lg:col-span-8 mt-10 xl:mt-10 space-y-4 sm:space-y-4 px-0 text-center sm:text-left">
           <span class="text-base text-gradient font-semibold uppercase"
