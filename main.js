d20763418513825586fbd947721cf433e7d8bb49
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 22:04:10 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,8 @@ import './assets/tables.scss'
 
 import Vue from 'vue'
 import VueParticles from './vue-particles'
-import App from './App'
+
 
 Vue.use(VueParticles)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app', 
-  components: { App },
-  template: '<App/>'
-})
