8492bd8e28850f66e101317852ee09f4efa867dc
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 22:30:02 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'normalize.css/normalize.css'
-import 'prismjs/themes/prism.css'
-import 'prismjs/themes/prism-okaidia.css'
+
 import './assets/tables.scss'
 
 import Vue from 'vue'
-import App from './App'
+
 
 import VueParticles from './vue-particles'
-import VuePrism from 'vue-prism'
-Vue.use(VueParticles, VuePrism)
+import App from './App'
+Vue.use(VueParticles)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+  el: '#app', 
+   components: { App },
+  template: '<App/>'
+
 })
