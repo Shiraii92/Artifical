6a2af9d970e09bdb44dbf2f6ee748f207910a499
Shiraii92 <alexanderlippmann@hotmail.de>
2023-01-10 18:24:15 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -53,7 +53,7 @@
             <img
               data-aos="fade-up"
               data-aos-once="true"
-              :src="require('~/assets/img/logo/beep.png')"
+              href="https://lovepik.com/images/png-intelligence.html">
               class="-mt-4"
               alt=""
             />
@@ -64,7 +64,7 @@
 
     <!-- Crypto statistic section -->
     <section
-      class="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-20 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12"
+      class="max-w-screen-xl mx-2 sm:mx-auto px-4 sm:px-6 lg:px-0 py-6 pb-50 sm:py-8 rounded-[2.25rem] sm:rounded-xl bg-white shadow-lg sm:shadow-md transform lg:-translate-y-12"
     >
       <div class="w-full flex flex-col lg:flex-row items-center justify-center">
         <LandingCryptoStatistic
